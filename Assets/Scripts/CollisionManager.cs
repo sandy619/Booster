054c10bc20ba18ab1d4bcbb1c639e2313f6ee3b6
sandy619 <31179398+sandy619@users.noreply.github.com>
2021-03-16 12:53:30 +0530

    AlphaBoosters

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionManager : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audsrc;
+    bool isActive = false;
+    
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip fail;
+    [SerializeField] ParticleSystem successParticle;
+    [SerializeField] ParticleSystem failParticle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audsrc = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Start":
+                Debug.Log("Start pad");
+                break;
+            case "Finish":
+                Debug.Log("You win");
+                
+                FinishSequence();
+                break;
+            default:
+                Debug.Log("LOSER");
+                
+                CrashSequence();
+                break;
+        }
+
+    }
+    void CrashSequence()
+    {
+        if (!isActive)
+        {
+            audsrc.PlayOneShot(fail);
+            failParticle.Play();
+
+            isActive = true;
+        }
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadScene", 1f);
+    }
+    void ReloadScene()
+    {   
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void FinishSequence()
+    {
+        if (!isActive)
+        {
+            audsrc.PlayOneShot(success);
+            successParticle.Play();
+            isActive = true;
+        }
+        GetComponent<Movement>().enabled = false;
+        //rb.freezeRotation=false;
+
+        Invoke("LoadNextScene", 1f);
+    }
+    void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex==SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+     Rigidbody rb;
+     AudioSource audsrc;
+    [SerializeField] float forca = 1000;
+    [SerializeField] float rotatespeed=100;
+    [SerializeField] AudioClip launch;
+    [SerializeField] ParticleSystem boost;
+    [SerializeField] ParticleSystem leftboost;
+    [SerializeField] ParticleSystem rightboost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audsrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up*forca*Time.deltaTime);
+            if (!audsrc.isPlaying)
+            {
+                audsrc.PlayOneShot(launch);
+                
+            }
+            if(!boost.isPlaying)
+                boost.Play();
+
+
+            //Debug.Log("moving up");
+        }
+        else
+        {
+            audsrc.Stop();
+            boost.Stop();
+        }
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.freezeRotation = true;
+            transform.Rotate(0, 0, 1*(Time.deltaTime)*(rotatespeed));
+            rb.freezeRotation = false;
+            if (!rightboost.isPlaying)
+            {
+                rightboost.Play();
+            }
+            //Debug.Log("rotate left");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            rb.freezeRotation = true;
+            transform.Rotate(0, 0, -1*(Time.deltaTime) * (rotatespeed));
+            rb.freezeRotation = false;
+            if (!leftboost.isPlaying)
+            {
+                leftboost.Play();
+            }
+            //Debug.Log("rotate right");
+        }
+        else
+        {
+            leftboost.Stop();
+            rightboost.Stop();
+        }
+    }
+}
